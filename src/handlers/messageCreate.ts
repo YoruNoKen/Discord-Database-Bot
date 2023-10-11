c201235c9741da62999fc6d40d5f95495e40220e
YoruNoKen <m.f.fetvaci@hotmail.com>
2023-10-11 03:12:49 +0300

    forgot to uncomment lol

--- src/handlers/messageCreate.ts
+++ src/handlers/messageCreate.ts
@@ -6,8 +6,8 @@ import { insertMessage } from "../utils";
 export const name = "messageCreate";
 export const execute = async (message: Message, client: MyClient) => {
   const ServerId = message.guildId;
-  // if (ServerId !== serverId) return console.error(`Erm.. I think the bot is being used somewhere that's not ${serverName}.`);
+  if (ServerId !== serverId) return console.error(`Erm.. I think the bot is being used somewhere that's not ${serverName}.`);
 
   // Insert messages into db
-  // await insertMessage(message);
+  await insertMessage(message);
 };
